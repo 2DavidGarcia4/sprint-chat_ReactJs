6f6b010db8dd86f5214dd9b3168437a556b45ad9
2DavidGarcia4 <gacub20ru@gmail.com>
2023-06-29 16:45:36 -0600

    disable strict mode & add global providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import GlobalProviders from './providers/globalProviders.tsx'
-// import '../utils/socket'
 import './styles/index.css'
 import './styles/dialogs.scss'
 import './styles/buttons.scss'
+import './utils/socket.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
