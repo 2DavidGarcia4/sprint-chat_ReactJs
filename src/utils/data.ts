bba6c46fecc971cf646861b657bd4719e97d8816
2DavidGarcia4 <gacub20ru@gmail.com>
2023-06-29 16:20:18 -0600

    update consts names to creaming case

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,13 +1,18 @@
 export const API_URL = 'http://localhost:2004'
 export const API_ENDPOINT = API_URL+'/api/v1/'
 
-export const statusTypes = {
-  '0': 'offline',
-  '1': 'online',
-  '2': 'idle',
-  '3': 'dnd',
+export const STATUS_NAMES = {
+  '0': 'Desconectado',
+  '1': 'Conectado',
+  '2': 'Ausente',
+  '3': 'No molestar',
   '4': 'say'
-}
+} as const
 
-export const appRoutes = ['/chats', '/me', '/friends', '/settings']
-export const oterRoutes = ['/login', '/register']
+export const APP_ROUTES = ['/chats', '/me', '/friends', '/settings']
+export const OTER_PATHS = ['/login', '/register']
+
+export const STORAGE_KEYS = {
+  token: 'secret',
+  status: 'defaultStatus'
+} as const
